e06e8e2776e2aee1ce26aa8d66fbf10408d0c423
Jacob <jsweeney@skillstorm.com>
2026-04-23 14:23:41 -0500

    Removing extra folder, files can still be found in commit history if needed

--- clientcompliance/src/main/java/com/skillstorm/repositories/ClientRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skillstorm.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.skillstorm.models.ClientRecord;
-
-public interface ClientRecordRepository extends MongoRepository<ClientRecord, String> {
-
-}
--- clientcompliance/src/main/java/com/skillstorm/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skillstorm.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.skillstorm.models.User;
-
-public interface UserRepository extends MongoRepository<User, String> {
-
-}
