1ea71dc2a7dcfd88d3ecbfaf6322231f0af76956
beerad00 <bradleykwateng@gmail.com>
2026-04-28 16:22:11 -0500

    Pair Programming to add JWT Authorization/Authencation

--- clientcompliance/src/main/java/com/skillstorm/repositories/UserRepository.java
+++ clientcompliance/src/main/java/com/skillstorm/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.skillstorm.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.skillstorm.models.User;
 
 public interface UserRepository extends MongoRepository<User, String> {
-
+	
+	Optional<User> findByEmail(String username); 
 }
---
+++ clientcompliance/src/main/java/com/skillstorm/security/JwtFilter.java
@@ -0,0 +1,59 @@
+package com.skillstorm.security;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService userDetailsService;
+
+    public JwtFilter(JwtUtil jwtUtil, CustomUserDetailsService uds) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = uds;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain)
+            throws ServletException, IOException {
+    	System.out.println("Filter hit");
+        String header = request.getHeader("Authorization");
+        System.out.println("header: "+ header);
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+            System.out.println("Token: "+ token);
+            try {
+                String username = jwtUtil.extractUsername(token);
+                
+                UserDetails user = userDetailsService.loadUserByUsername(username);
+
+                UsernamePasswordAuthenticationToken auth =
+                        new UsernamePasswordAuthenticationToken(
+                                user, null, user.getAuthorities()
+                        );
+                System.out.println("Create Auth token: " + auth);
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+                
+                System.out.println("Security Context: " + SecurityContextHolder.getContext().getAuthentication());
+            } catch (Exception e) {
+                System.out.println("Caught Error: "+ e.getMessage());
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+}
